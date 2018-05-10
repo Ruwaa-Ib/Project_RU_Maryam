f075c3a0b0e3cf61fe05beafcf04cc458651e198
Ruwaa-Ib <ruwaa.ibrahem@gmail.com>
2018-05-10 23:54:36 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+### This is the main file
+#####   #####    ##### File Parsing #####   #####    ##### Maryam
+
+
+#####   #####    ##### Machine Learning #####   #####    ##### Maryam & RU
+
+
+#####   #####    ##### Markov Model #####   #####    ##### RU
+import MM
+
+
+#####   #####    ##### Decesion #####   #####    ##### RU
