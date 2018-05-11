4f492369de7a1e0d8db53e79b766fc529e999fe8
Ruwaa-Ib <ruwaa.ibrahem@gmail.com>
2018-05-11 21:37:41 +0200

    NLS_seq_module.py

--- NLS_seq_module.py
+++ NLS_seq_module.py
@@ -1,19 +1,16 @@
 # This Module contains all the known NLS
 NLS_list = ['PKKKRKV',          #DOI: 10.1002/iub.194
-            'KRPAATKKAGQAKKKK1',
+            'KRPAATKKAGQAKKKK',
             'KRPMNAFIVWSRDQRRK',
             'RPRRK',
             'SSNFGPMKGGNRFFRSSGPY',
             'RSGGNHRRNGRGGRGGYNRRNNGYHPY',
             'LRLTLLELVRRLNGNG',
             'GKISKHWTGI',
-            'SKKRSLED.{0,23}PPRSKKRI',
             'RQARRNRRRRWR',
             'GRKKRRQRRRAP',
-            'MPKTRRRPRRSQRKRPPT',
-            'KSAKISKPLH.{0,29}KNKEISMP',
-            'KVTKNKS.{0,7}KRRGKPGP',
-            'TAKRS.{0,35}KKKGSKTS']
+            'MPKTRRRPRRSQRKRPPT'
+            ]
 
 
 ##for element in NLS_list:
